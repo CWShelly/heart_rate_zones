8d01b628d454bdf43c824cffbf80f02532a51771
Shelly Yusuf <ShellyNYusuf@gmail.com>
2018-04-13 00:02:09 -0700

    rounding

--- public/src/components/DisplayZones.js
+++ public/src/components/DisplayZones.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DisplayZones = (props)=>(
 
- 
+
   <div className="displayContainer">
   <p className="myZones-bigger">{props.message}</p>
   {props.age > 0 && <p className="myZones">Your age: {props.age}</p>}
@@ -10,7 +10,7 @@ const DisplayZones = (props)=>(
   {props.lower > 0 &&   <p className="myZones">Your recommended lower range: {props.lower}</p>}
   {props.higher > 0 && <p className="myZones"> Your recommended upper range: {props.higher}</p>}
   {props.max > 0 &&   <p className="myZones"> Your max heart rate: {props.max}</p>}
-  {props.hrr >0 &&   <p className="myZones"> Your heart rate reserve: {props.hrr}</p>}
+ 
   </div>
 )
 
