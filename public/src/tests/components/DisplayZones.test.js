c55a4f8012eaad4e6cd65b3a6a5af1acf706826c
Shelly Yusuf <ShellyNYusuf@gmail.com>
2018-04-22 17:32:47 -0700

    adding DisplayZone test and fixtures file

---
+++ public/src/tests/components/DisplayZones.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import DisplayZones from '../../components/DisplayZones';
+import { emptyInfo, dummyInfo } from '../fixtures/heart_rate_info';
+
+
+test('should correctly display DisplayZones page ',()=>{
+  const wrapper = shallow(<DisplayZones />);
+  expect(wrapper).toMatchSnapshot()
+})
+
+test('should handle DisplayZones data when nothing is added', ()=>{
+    const wrapper = shallow(<DisplayZones message={emptyInfo.message}
+      age={emptyInfo.age} mod_lower={emptyInfo.mod_lower}
+      vig_higher={emptyInfo.vig_higher} max={emptyInfo.max} />);
+        expect(wrapper).toMatchSnapshot();
+})
+
+test('should handle DisplayZones data when info is added', ()=>{
+    const wrapper = shallow(<DisplayZones message={dummyInfo.message}
+      age={dummyInfo.age} mod_lower={dummyInfo.mod_lower}
+      vig_higher={dummyInfo.vig_higher} max={dummyInfo.max} />);
+      xpect(wrapper).toMatchSnapshot();
+})
---
+++ public/src/tests/fixtures/heart_rate_info.js
@@ -0,0 +1,18 @@
+
+const emptyInfo ={
+  message: '',
+  age: 0,
+  mod_lower: 0,
+  vig_lower: 0,
+  max: 0
+}
+
+const dummyInfo = {
+  message: 'test message',
+  age: 44,
+  mod_lower: 100,
+  vig_lower: 150,
+  max: 220-44
+}
+
+export { emptyInfo, dummyInfo };
